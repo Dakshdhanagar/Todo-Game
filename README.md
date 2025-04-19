053627adc4b8a723c8897abdfa54532627375242
Daksh Dhanagar <dakshoffical770@gmail.com>
2025-04-19 23:30:58 +0530

    Bubbling added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Todo-Game
 THis is a simple game in which we only adding and deleting the daily life routine in a list.
+
+# adding new concept for deleting the newly added elements that's called "bubbling"
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let btn =document.querySelector("button");
 
 let ul=document.querySelector("ul");
-let input =document.querySelector("input");
+let inp =document.querySelector("input");
 
 
 btn.addEventListener("click", function () {
     let item = document.createElement("li");
-    item.innerText = input.val;
+    item.innerText = inp.value;
 
     let delBtn= document.createElement("button");
     delBtn.innerText="delete";
@@ -17,17 +17,33 @@ btn.addEventListener("click", function () {
     item.appendChild(delBtn);
     ul.appendChild(item);
    
-    input.val="";
+    inp.value = "";
 } );
 
-let dts =document.querySelectorAll(".delete");
-for( delBtn of dts){
-    delBtn.addEventListener("click",function () {
-        let par= this.parentElement;
-        console.log(par);
-        console.log("working");
-        par.remove();
+//event bubbling with usig ul
+//now by using event in callback we know which button was clicked
+//with the help of event.target
+
+ul.addEventListener("click", function (event) {
+    
+    // console.dir(event.target.nodeName);
+     if(event.target.nodeName == "BUTTON"){
+        let listitem = event.target.parentElement;
+        listitem.remove();
+        console.log("deleted")
+    }
+
+
+});
+
+// let dts =document.querySelectorAll(".delete");
+// for( delBtn of dts){
+//     delBtn.addEventListener("click",function () {
+//         let par= this.parentElement;
+//         console.log(par);
+//         console.log("working");
+//         par.remove();
        
-    });
-}
+//     });
+// }
 
